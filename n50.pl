7fdfcf170858997b446f855b8c6f76403eeab5e3
reubwn <reubennowell@gmail.com>
2016-07-29 14:22:40 +0100

    update n50.pl

--- n50.pl
+++ n50.pl
@@ -2,23 +2,29 @@
 
 use strict;
 use warnings;
+
 use Bio::Seq;
 use Bio::SeqIO;
 use List::Util qw/sum/;
 
 my $usage = "
-	Prints assembly info for fasta file(s)
-	USAGE: n50.pl FASTA1 [FASTA2 ...]\n
+  Prints assembly info for fasta file(s)
+  USAGE: n50.pl <FASTA1> [FASTA2... ]\n
 ";
 
-die $usage if @ARGV == 0;
+die $usage if $ARGV[0] =~ /-h/;
 
 for my $i (0 .. $#ARGV){
 
 	my (@scaff_lengths);
-	my ($tot,$num200,$num1kb,$num10kb,$ass_N50,$ass_N90,$ass_L50,$ass_L90,$nns,$gc) = (0,0,0,0,0,0,0,0,0,0);
+	my ($tot,$num200,$num500,$num1kb,$num10kb,$ass_N50,$ass_N90,$ass_L50,$ass_L90,$nns,$gc) = (0,0,0,0,0,0,0,0,0,0,0);
 
-	my $in = Bio::SeqIO->new( -file => $ARGV[$i], -format => 'fasta' );
+	my $in;
+	if ($ARGV[0] eq "-"){
+		$in  = Bio::SeqIO->new( -fh => \*STDIN, -format => 'fasta' );
+	} else {
+		$in  = Bio::SeqIO->new( -file => $ARGV[$i], -format => 'fasta' );
+	}
 	while (	my $seq = $in->next_seq() ){
 		push (@scaff_lengths, $seq->length());
 		## count NNNs
@@ -35,6 +41,7 @@ for my $i (0 .. $#ARGV){
 
 	foreach my $len (@sorted_scaffs){
 		$num200++ if $len > 200;
+		$num500++ if $len > 500;
 		$num1kb++ if $len > 1000;
 		$num10kb++ if $len > 10000;
 	}
@@ -59,30 +66,31 @@ for my $i (0 .. $#ARGV){
 	}
 
 	print "
-		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        	$ARGV[$i]
-        	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        	Total span: ".commify($span)."
-        	Total number sequences: ".commify(scalar(@sorted_scaffs))."
-                	\>200nt: ".commify($num200)."
-                	\>1kb  : ".commify($num1kb)."
-                	\>10kb : ".commify($num10kb)."
-        	Smallest: ".commify($sorted_scaffs[-1])."
-	        Longest : ".commify($sorted_scaffs[0])."
-		
-		\%GC : ".percentage($gc,$span)."
-        	NNNs: ".commify($nns)." (".percentage($nns,$span).")
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+$ARGV[$i]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Total span: ".commify($span)."
+Total number sequences: ".commify(scalar(@sorted_scaffs))."
+  \>200nt: ".commify($num200)."
+  \>500nt: ".commify($num500)."
+  \>1kb  : ".commify($num1kb)."
+  \>10kb : ".commify($num10kb)."
+Smallest: ".commify($sorted_scaffs[-1])."
+Longest : ".commify($sorted_scaffs[0])."
+	
+\%GC : ".percentage($gc,$span)."
+NNNs: ".commify($nns)." (".percentage($nns,$span).")
 
-        	N50: ".commify($ass_N50)."
-        	L50: ".commify($ass_L50)."
-        	N90: ".commify($ass_N90)."
-        	L90: ".commify($ass_L90)."
-        	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        	* N50 = length of scaffold at 50% genome
-        	* L50 = number of scaffolds to get to N50
-        	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	\n";
+N50: ".commify($ass_N50)."
+num: ".commify($ass_L50)."
+N90: ".commify($ass_N90)."
+num: ".commify($ass_L90)."
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*N50 = length of scaffold at 50\% genome
+*num = num scaffolds to get to N50
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
 }
+
 sub commify {
     my $text = reverse $_[0];
     $text =~ s/(\d\d\d)(?=\d)(?!\d*\.)/$1,/g;
