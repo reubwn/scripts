d4528e4a878a5f41f18092d370603b3280804737
reubwn <reubennowell@gmail.com>
2016-08-05 15:49:04 +0100

    code restructure and sam output

--- bam2tab.pl
+++ bam2tab.pl
@@ -14,7 +14,7 @@ bam2tab.pl
 Converts SAM/BAM file to TAB format used in SSPACE.
 NOTE: requires samtools in \$PATH and SAM/BAM sorted by readname (-n option in samtools sort).
 
-USAGE: bam2tab.pl -b <bam_file> [-s <sam_file>] [-d mapping_distance] [-o output_table.txt]
+USAGE: bam2tab.pl -i <bam_file> [-d mapping_distance] [-s] [-o output_table.txt]
 
 OPTIONS:
   -i|--in            [FILE]  : SAM/BAM file [required]
@@ -22,16 +22,18 @@ OPTIONS:
   -d|--edge_distance [INT]   : filter based on minimum mapping distance from contig edge [default: 0]
   -s|--same_contig           : filter reads mapping to the same contig (i.e., are redundant) [default: no]
   -o|--output        [FILE]  : output file name [default: mapping_table.txt]
+  -t|--outtype       [STR]   : Output format: 'table' => table [default], 'sam' => sam format
   -h|--help                  : prints this help message
 
 EXAMPLES:
   (1) Get reads that map to within 1 kb of contig edges, discard any that map to the same contig:
-      >> bam2tab.pl -b mapping.bam -d 1000 -s -o mapping_table.1kb.txt
+      >> bam2tab.pl -i mapping.bam -d 1000 -s -o mapping_table.1kb.txt
 \n";
 
 ## params with defaults
 my $edge_distance = 0;
 my $output = "mapping_table.txt";
+my $outtype = "table";
 
 ## other args
 my ($sam_file,$fasta,$same,$help);
@@ -42,6 +44,7 @@ GetOptions (
   'edge_distance|d:i'  => \$edge_distance,
   'same_contig|s'      => \$same,
   'output|o:s'         => \$output,
+  'outtype|t:s'        => \$outtype,
   'help|h'             => \$help,
 );
 
@@ -55,19 +58,37 @@ if (system("samtools view &>/dev/null")==-1){
 
 ## get sequence lengths from fasta
 my %lengths;
-print "\nFetching contig lengths from $fasta... ";
+print "\nFetching contig lengths from $fasta...\n";
 my $in = Bio::SeqIO->new ( -file => $fasta, -format => "fasta" );
 while ( my $seq_obj = $in->next_seq() ){
   $lengths{($seq_obj->display_id())} = ($seq_obj->length());
 }
-print scalar(keys %lengths)." sequences\n";
+print "Found ".commify(scalar(keys %lengths))." contigs\n\n";
 
+## open SAM/BAM file, only include proper pairs
+my $SAM;
+if ($outtype =~ m/sam/i){
+  $output = "mapping_table.sam";
+  open ($SAM, "samtools view -h -f1 -F3340 $sam_file |") or die $!;
+} else {
+  open ($SAM, "samtools view -f1 -F3340 $sam_file |") or die $!;
+}
+
+my ($processed,$printed) = (0,0);
 open (my $OUT, ">$output") or die $!;
 
-## open SAM/BAM file, skip SAM headers, only include proper pairs
-open (my $SAM, "samtools view -f1 -F3340 $sam_file |") or die $!;
 while (<$SAM>){
-  chomp;
+
+  ## skip headers unless print to SAM
+  if ($_ =~ m/^\@/){
+    if ($outtype =~ m/sam/i){
+      print $OUT $_;
+      next;
+    } else {
+      next;
+    }
+  }
+
   my @F = split (/\t/, $_); ## first read
   my @R = split (/\t/, <$SAM>); ## second read
 
@@ -76,47 +97,99 @@ while (<$SAM>){
     next;
   }
 
-  ############################ calculate distance from contig edge
+  ############################ calculate distances from contig edge
 
-  my ($fragment_leftmost,$fragment_rightmost,$distance_from_left,$distance_from_right);
+  #my ($fragment_leftmost,$fragment_rightmost,$distance_from_left,$distance_from_right);
+  my ($start1,$end1,$contig1, $start2,$end2,$contig2);
+  my ($edge1,$edge2) = (0,0);
 
-  ## read is on forward strand, mate is on reverse strand
+  ## read1 is on forward strand, mate is on reverse strand
   if ($F[1]&32){
 
     ## calculate aln length from CIGAR
-    my ($aln_length_F,$aln_length_R) = (0,0);
-    $F[5] =~ s/(\d+)[MX=DN]/$aln_length_F+=$1/eg;
-    $R[5] =~ s/(\d+)[MX=DN]/$aln_length_R+=$1/eg;
+    my ($aln_length_1,$aln_length_2) = (0,0);
+    my ($cigar1,$cigar2) = ($F[5],$R[5]);
+    $cigar1 =~ s/(\d+)[MX=DN]/$aln_length_1+=$1/eg;
+    $cigar2 =~ s/(\d+)[MX=DN]/$aln_length_2+=$1/eg;
 
-    ## calculate outermost fragment positions
-    $fragment_leftmost = $F[3];
-    $fragment_rightmost = $R[3] + $aln_length_R - 1;
+    ## contig2
+    ## ==========+++++++++++++
+    ##    3'<----5'
+    ##      read2(R)            contig1
+    ##                          ++++++++==================
+    ##                                5'---->3'
+    ##                                  read1(F)
+    ##
+    ## the regions marked +++ must be > $edge_distance
 
-    ## calculate distance from contig edges
-    $distance_from_left = $fragment_leftmost;
-    $distance_from_right = $lengths{$R[2]} - $fragment_rightmost;
+    ## get start/end coords for both reads
+    $start1 = $F[3];
+    $end1 = $F[3] + $aln_length_1;
+    $contig1 = $F[2];
+    $start2 = $R[3] + $aln_length_2;
+    $end2 = $R[3];
+    $contig2 = $R[2];
 
-    ## print to $OUT if $distance_from_left AND $distance_from_right > $edge_distance
-    if ( ($distance_from_left > $edge_distance) && ($distance_from_right > $edge_distance) ){
-      print "$F[2]\t$fragment_leftmost\t".($fragment_leftmost + $aln_length_F - 1)."\t$R[2]\t".($fragment_rightmost - $aln_length_R + 1)."\t$fragment_rightmost\n";
-    }
+    $edge1 = $start1;
+    $edge2 = $lengths{$contig2} - $start2;
 
-  ## read is on reverse strand, mate is on forward strand
+  ## read1 is on reverse strand, mate is on forward strand
   } elsif ($F[1]&16) {
-    my ($aln_length_F,$aln_length_R) = (0,0);
-    $F[5] =~ s/(\d+)[MX=DN]/$aln_length_F+=$1/eg;
-    $R[5] =~ s/(\d+)[MX=DN]/$aln_length_R+=$1/eg;
+    my ($aln_length_1,$aln_length_2) = (0,0);
+    my ($cigar1,$cigar2) = ($F[5],$R[5]);
+    $cigar1 =~ s/(\d+)[MX=DN]/$aln_length_1+=$1/eg;
+    $cigar2 =~ s/(\d+)[MX=DN]/$aln_length_2+=$1/eg;
 
-    $fragment_leftmost = $R[3];
-    $fragment_rightmost = $F[3] + $aln_length_F - 1;
+    ## contig1
+    ## ==========+++++++++++++
+    ##    3'<----5'
+    ##      read1(F)            contig2
+    ##                          ++++++++==================
+    ##                                5'---->3'
+    ##                                     read2(R)
+    ##
+    ## the regions marked +++ must be > $edge_distance
 
-    $distance_from_left = $fragment_leftmost;
-    $distance_from_right = $lengths{$F[2]} - $fragment_rightmost;
+    ## get start/end coords for both reads
+    $start1 = $F[3] + $aln_length_1;
+    $end1 = $F[3];
+    $contig1 = $F[2];
+    $start2 = $R[3];
+    $end2 = $R[3] + $aln_length_2;
+    $contig2 = $R[2];
 
-    if ( ($distance_from_left > $edge_distance) && ($distance_from_right > $edge_distance) ){
-      print "$R[2]\t$fragment_leftmost\t".($fragment_leftmost + $aln_length_R - 1)."\t$F[2]\t".($fragment_rightmost - $aln_length_F + 1)."\t$fragment_rightmost\n";
-    }
+    $edge1 = $lengths{$contig1} - $start1;
+    $edge2 = $start2;
   }
+
+  ## print to $OUT if both $edge1 && $edge2 > $edge_distance
+  if (($edge1 > $edge_distance) && ($edge2 > $edge_distance)){
+    if ($outtype =~ m/sam/i){
+      print $OUT join "\t", @F;
+      print $OUT join "\t", @R;
+    } else {
+      print $OUT join "\t", $contig1,$start1,$end1,$contig2,$start2,$end2,"\n";
+    }
+    $printed++;
+  }
+
+  ## progress
+  $processed++;
+  if ($processed % 100000 == 0){
+    print "\rProcessed ".commify($processed)." pairs...";
+    $| = 1;
+  }
+}
+
+print "\rProcessed ".commify($processed)." pairs\n";
+print "Printed ".commify($printed)." pairs\n\n";
+
+###################### sub-routines
+
+sub commify {
+  my $text = reverse $_[0];
+  $text =~ s/(\d\d\d)(?=\d)(?!\d*\.)/$1,/g;
+  return scalar reverse $text;
 }
 
 __END__
