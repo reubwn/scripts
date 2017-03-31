b79129be5be6609b037a4d4cd0c4a3e51ffbbf89
reubwn <reubennowell@gmail.com>
2017-03-31 17:57:09 +0100

    add pseudochr option

--- window_cov.pl
+++ window_cov.pl
@@ -8,18 +8,26 @@ my $usage = "
 SYNOPSIS
   Calculates average of a window, e.g. for averaging coverage across a per-site (-d) genomeCov file.
 
+OPTIONS
+  -i|--in        [FILE]: infile
+  -o|--out       [FILE]: outfile (default = <infile>.<windowsize>.txt)
+  -w|--window    [INT] : window size to average across (default = 1000)
+  -p|--pseudochr       : also print 1..n across all scaffolds
+  -h|--help            : this
+
 USAGE
-  window_cov.pl -i genomeCov.txt -w 1000 -o genomeCov.1kb.txt
+  window_cov.pl -i genomeCov.txt -w 1000 -o genomeCov.1000.txt
 \n";
 
-my ($infile, $outfile, $help);
+my ($infile, $outfile, $pseudochr, $help);
 my $window = 1000;
 
 GetOptions (
-  'in|i=s'     => \$infile,
-  'out|o:s'    => \$outfile,
-  'window|w:i' => \$window,
-  'help|h'     => \$help
+  'in|i=s'      => \$infile,
+  'out|o:s'     => \$outfile,
+  'window|w:i'  => \$window,
+  'pseudochr|p' => \$pseudochr,
+  'help|h'      => \$help
 );
 
 die $usage if $help;
@@ -37,7 +45,7 @@ while (<$IN>) {
   push (@string, $_); ##for later printing
   my @F = split(/\s+/, $_);
   $scaff_lengths{$F[0]}++; ##get length of each scaffold
-  print STDERR "\r[INFO] Scaffold $F[0]..." if ($. % 1000 == 0); $|=1;
+  print STDERR "\r[INFO] Analysing scaffold $F[0]..." if ($. % 1000 == 0); $|=1;
 
   if ($scaff_lengths{$F[0]} % $window == 0) { ## push if window is reached
     $sum += $F[2]; ##add last coverage
@@ -72,8 +80,14 @@ print STDERR "\n[INFO] Finished averaging...\n[INFO] Printing to $outfile\n";
 
 ##print to reannotated file, each site will get average coverage across the specified window
 open (my $OUT, ">$outfile") or die "Cannot open $outfile: $!\n";
+my $n = 1;
 for my $j (0..$#string) {
-  print $OUT join("\t", $string[$j], $averages[$j], "\n");
+  if ($pseudochr) {
+    print $OUT join("\t", $string[$j], $n, $averages[$j], "\n");
+    $n++;
+  } else {
+    print $OUT join("\t", $string[$j], $averages[$j], "\n");
+  }
 }
 close $OUT;
 print STDERR "[INFO] Finished\n\n";
