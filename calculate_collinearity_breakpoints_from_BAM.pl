26ac3680024f2a6e9a1ba381c8ead4d7e8a2c804
reubwn <reubennowell@gmail.com>
2017-06-14 12:11:23 +0100

    add bam

--- calculate_collinearity_breakpoints_from_BAM.pl
+++ calculate_collinearity_breakpoints_from_BAM.pl
@@ -14,23 +14,28 @@ SYNOPSIS
 OUTPUT
 
 OPTIONS
-  -a|--bam [FILE] : BAM file
-  -b|--bed        : windows in BED format
-  -g|--genome     : genome file that defines the expected chromosome order in the input files
-  -o|--out        : outfile name
-  -h|--help       : print this message
+  -a|--bam    [FILE] : BAM file (required)
+  -r|--region [STR]  : specify a region(s) to analyse (default: all)
+  -b|--bed    [FILE] : windows in BED format (required)
+  -g|--genome [FILE] : BEDtools style genome lengths file (required)
+  -i|--insert [INT]  : insert size threshold; proportion of pairs with ISIZE >= this value reported (default: 500)
+  -o|--out           : outfile name (default: STDOUT)
+  -h|--help          : print this message
 
 USAGE
 
 \n";
 
-my ($bam,$bed,$genome,$help,$debug);
+my ($bam,$region,$bed,$genome,$help,$debug);
+my $insert = 500;
 my $n = 1;
 
 GetOptions (
   'a|bam=s'  => \$bam,
+  'r|region:s' => \$region,
   'b|bed=s'  => \$bed,
   'g|genome=s' => \$genome,
+  'i|insert'  => \$insert,
   'h|help'   => \$help,
   'd|debug'  => \$debug
 );
@@ -39,6 +44,7 @@ die $usage if $help;
 die $usage unless ($bam && $bed && $genome);
 
 print STDERR "[INFO] BAM file: $bam\n";
+print STDERR "[INFO] Region specified: $region\n" if ($region);
 print STDERR "[INFO] Windows file: $bed\n";
 print STDERR "[INFO] Genome file: $genome\n";
 print STDOUT join (
@@ -46,9 +52,9 @@ print STDOUT join (
   "CHROM\tSTART\tEND",#$window,
   "TOTAL",#$total,
   "SAME",#$same,
-  "S/T",#($same/$total),
+  "S:T",#($same/$total),
   "INSERT",#$insert,
-  "I/T",#($insert/$total),
+  "I:T",#($insert/$total),
   "INSERT_AVG",#(sum(@insert_arr)/scalar(@insert_arr)),
   "\n"
 );
@@ -57,14 +63,15 @@ open (my $BED, $bed) or die "[ERROR] Cannot open $bed: $!\n";
 while (my $window = <$BED>) {
   #print STDERR "\r[INFO] Working on window \#$n: $_";$| = 1;
   `printf "$window" > tmp.bed`;
-  my ($SAM,$total,$same,$insert,@insert_arr,$insert_avg);
-  open($SAM, "bedtools intersect -sorted -g $genome -a $bam -b tmp.bed | samtools view - |");#`bedtools intersect -sorted -g $genome -a $bam -b <(printf "$_") | samtools view - | perl -lane 'if($F[6]eq"="){if($F[8]>500){$insert++};$same++;$total++}else{$total++}END{print "$total\t$same\t".($same/$total)."\t$insert\t".($insert/$total)}'`;
+  my ($total,$same,$insert,$insert_avg) = (0,0,0,0);
+  my @insert_arr;
+  open(my $SAM, "samtools view -b $bam $region | bedtools intersect -sorted -g $genome -a stdin -b tmp.bed | samtools view - |");#`bedtools intersect -sorted -g $genome -a $bam -b <(printf "$_") | samtools view - | perl -lane 'if($F[6]eq"="){if($F[8]>500){$insert++};$same++;$total++}else{$total++}END{print "$total\t$same\t".($same/$total)."\t$insert\t".($insert/$total)}'`;
   while (<$SAM>) {
     my @F = split (/\s+/, $_);
     if ($F[6] eq "=") { ##mate on same scaffold
       $same++;
       $total++;
-      $insert++ if ($F[8] > 500);
+      $insert++ if ($F[8] > $insert);
       push (@insert_arr, abs($F[8]));
     } else {
       $total++;
