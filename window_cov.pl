44c053e7865b6552f781aba5b4f521443f9ca798
reubwn <reubennowell@gmail.com>
2017-07-06 20:59:35 +0100

    fix script

--- window_cov.pl
+++ window_cov.pl
@@ -50,20 +50,23 @@ print STDERR "[INFO] Window size: $window\n";
 print STDERR "[INFO] Flatten set to TRUE\n" if $flatten;
 print STDERR "[INFO] Input is gzipped\n" if $gzip;
 unless ($outfile) { $outfile = "$infile.$window.txt" };
-my ($IN, $sum);
-my (%scaff_lengths, %seen, %v);
-my (@string, @averages);
+
+my ($cumulative_coverage_sum, $prev_cumulative_coverage_sum);
+my (%cumulative_scaffold_length, %seen, %v);
+my (@E);
+
+my $IN;
 if ($gzip) {
-  open ($IN, "zcat $infile |") or die "Cannot open $infile: $!\n";
+  open ($IN, "zcat $infile |") or die "[ERROR] Cannot open $infile: $!\n";
   $outfile =~ s/\.gz//;
 } else {
-  open ($IN, $infile) or die "Cannot open $infile: $!\n";
+  open ($IN, $infile) or die "[ERROR] Cannot open $infile: $!\n";
 }
-open (my $OUT, ">$outfile") or die "Cannot open $outfile: $!\n";
+open (my $OUT, ">$outfile") or die "[ERROR] Cannot open $outfile: $!\n";
 if ($vcffile) {
   print STDERR "[INFO] Analysing VCF file: $vcffile\n";
   my (%e, $cwin);
-  open (my $VCF, $vcffile) or die "Cannot open file $vcffile: $!\n";
+  open (my $VCF, $vcffile) or die "[ERROR] Cannot open file $vcffile: $!\n";
   while (<$VCF>) {
     chomp;
     my @F = split(/\s+/, $_);
@@ -97,64 +100,98 @@ if ($vcffile) {
 }
 
 ## process genomeCov file
-while (<$IN>) {
+LINE: while (<$IN>) {
   chomp;
-  push (@string, $_); ##for later printing
   my @F = split(/\s+/, $_);
-  $scaff_lengths{$F[0]}++; ##get length of each scaffold
   print STDERR "\r[INFO] Analysing scaffold $F[0]..." if ($. % 5000 == 0); $|=1; ##just print info every 5000 bases
 
-  if ($scaff_lengths{$F[0]} % $window == 0) { ##push if window is reached
-    $sum += $F[2]; ##add last coverage
-    for (1..$window) {
-      push (@averages, ($sum/$window)) ##push to array of windowsize, so each base gets the average value
+  #$cumulative_scaffold_length{$F[0]}++; ##increment length of each scaffold
+  ## add coverage at current site to cumulative_coverage_sum:
+  $cumulative_coverage_sum += $F[2];
+
+  ## print if window size is reached:
+  if ( $F[1] % $window == 0) {
+    if ($vcffile) {
+      my $nsnps;
+      if (exists($v{$F[0]}{$F[1]})) { $nsnps = $v{$F[0]}{$F[1]} } else { $nsnps = 0 };
+      print $OUT join("\t", $F[0], $F[1], ($cumulative_coverage_sum/$window), $nsnps, ($nsnps/$window), "\n");
+    } else {
+      print $OUT join("\t", $F[0], $F[1], ($cumulative_coverage_sum/$window),"\n");
     }
-    if ($flatten) {
-      if ($vcffile) {
-        my $nsnps;
-        if (exists($v{$F[0]}{$F[1]})) { $nsnps = $v{$F[0]}{$F[1]} } else { $nsnps = 0 };
-        print $OUT join("\t", $F[0], $F[1], ($sum/$window), $nsnps, ($nsnps/$window), "\n")
-      } else {
-        print $OUT join("\t", $F[0], $F[1], ($sum/$window),"\n");
-      }
+
+    ## reset cumsum to 0:
+    $cumulative_coverage_sum = 0;
+
+  ## OR its a new scaffold (not seen already) OR eof:
+  } elsif ( (!exists($seen{$F[0]})) || (eof) ) {
+    if ($vcffile) {
+      my $nsnps;
+      if (exists($v{$E[0]}{$E[1]})) { $nsnps = $v{$E[0]}{$E[1]} } else { $nsnps = 0 }; ##WARNING: THIS MIGHT NEVER WORK.....!!!!!
+      print $OUT join("\t", $E[0], $E[1], ($prev_cumulative_coverage_sum/($E[1] % $window)), $nsnps, ($nsnps/($E[1] % $window)), "\n"); ##WARNING: NSNPS and DENSITY will always be zero for this last window.... IS THIS OK
+    } else {
+      print $OUT join("\t", $E[0], $E[1], ($prev_cumulative_coverage_sum/($E[1] % $window)),"\n");
     }
-    $sum = 0; ##reset
-   #--0;'''''''''''''''''v  b v hb jb nb   vv                   v cfv hfgyh                                        c  µ ── ·─·─ n baby mamie's line of code :-)
-  } else {
-    if (eof) { ## special case for eof
-      $sum += $F[2];
-      for (1..($scaff_lengths{$F[0]} % $window)) {
-        push (@averages, ($sum/($scaff_lengths{$F[0]} % $window)));
-      }
-      if ($flatten) {
-        if ($vcffile) {
-          my $nsnps;
-          if (exists($v{$F[0]}{$F[1]})) { $nsnps = $v{$F[0]}{$F[1]} } else { $nsnps = 0 };
-          print $OUT join("\t", $F[0], $F[1], ($sum/($scaff_lengths{$F[0]} % $window)), $nsnps, ($nsnps/($scaff_lengths{$F[0]} % $window)), "\n")
-        } else {
-          print $OUT join("\t", $F[0], $F[1], ($sum/($scaff_lengths{$F[0]} % $window)),"\n");
-        }
-      }
-    }
-    $sum += $F[2];
+
+    ## reset cumsum to 0 then begin again:
+    $cumulative_coverage_sum = 0;
+    $cumulative_coverage_sum += $F[2];
   }
-  $seen{$F[0]}=();
+
+  @E = @F; ##store current line in variable accessible to next iteration
+  $prev_cumulative_coverage_sum = $cumulative_coverage_sum; ##store current cumsum in variable accessible to next iteration
+  $seen{$F[0]}=(); ##see the scaffold
+
+  # if ($cumulative_scaffold_length{$F[0]} % $window == 0) { ##push or print if window is reached
+  #   $sum += $F[2]; ##add last coverage
+  #   for (1..$window) {
+  #     push (@averages, ($sum/$window)) ##push to array of windowsize, so each base gets the average value
+  #   }
+  #   if ($flatten) {
+  #     if ($vcffile) {
+  #       my $nsnps;
+  #       if (exists($v{$F[0]}{$F[1]})) { $nsnps = $v{$F[0]}{$F[1]} } else { $nsnps = 0 };
+  #       print $OUT join("\t", $F[0], $F[1], ($sum/$window), $nsnps, ($nsnps/$window), "\n")
+  #     } else {
+  #       print $OUT join("\t", $F[0], $F[1], ($sum/$window),"\n");
+  #     }
+  #   }
+  #   $sum = 0; ##reset
+  #  #--0;'''''''''''''''''v  b v hb jb nb   vv                   v cfv hfgyh                                        c  µ ── ·─·─ n baby mamie's line of code :-)
+  # } else {
+  #   if (eof) { ## special case for eof
+  #     $sum += $F[2];
+  #     for (1..($cumulative_scaffold_length{$F[0]} % $window)) {
+  #       push (@averages, ($sum/($cumulative_scaffold_length{$F[0]} % $window)));
+  #     }
+  #     if ($flatten) {
+  #       if ($vcffile) {
+  #         my $nsnps;
+  #         if (exists($v{$F[0]}{$F[1]})) { $nsnps = $v{$F[0]}{$F[1]} } else { $nsnps = 0 };
+  #         print $OUT join("\t", $F[0], $F[1], ($sum/($cumulative_scaffold_length{$F[0]} % $window)), $nsnps, ($nsnps/($cumulative_scaffold_length{$F[0]} % $window)), "\n")
+  #       } else {
+  #         print $OUT join("\t", $F[0], $F[1], ($sum/($cumulative_scaffold_length{$F[0]} % $window)),"\n");
+  #       }
+  #     }
+  #   }
+  #   $sum += $F[2];
+  # }
+  # $seen{$F[0]}=();
 }
 close $IN;
 print STDERR "\n[INFO] Finished averaging...\n[INFO] Printing to $outfile\n";
 
-##print to reannotated file, each site will get average coverage across the specified window
-my $n = 1;
-unless ($flatten) {
-  for my $j (0..$#string) {
-    if ($pseudochr) {
-      print $OUT join("\t", $string[$j], $n, $averages[$j], "\n");
-      $n++;
-    } else {
-      print $OUT join("\t", $string[$j], $averages[$j], "\n");
-    }
-  }
-}
+# ##print to reannotated file, each site will get average coverage across the specified window
+# my $n = 1;
+# unless ($flatten) {
+#   for my $j (0..$#string) {
+#     if ($pseudochr) {
+#       print $OUT join("\t", $string[$j], $n, $averages[$j], "\n");
+#       $n++;
+#     } else {
+#       print $OUT join("\t", $string[$j], $averages[$j], "\n");
+#     }
+#   }
+# }
 close $OUT;
 print STDERR "[INFO] Finished on ".`date`."\n\n";
 
