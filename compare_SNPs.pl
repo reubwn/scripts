7347ce034e2c0def49f795572324e49eb76b3a74
reubwn <reubennowell@gmail.com>
2017-12-06 12:08:33 +0000

    update script

--- compare_SNPs.pl
+++ compare_SNPs.pl
@@ -5,6 +5,7 @@
 use strict;
 use warnings;
 
+use Bio::SeqIO;
 use Getopt::Long;
 use Sort::Naturally;
 
@@ -12,17 +13,19 @@ my $usage = "
 OPTIONS:
   -1|--file1 [FILE] : SNP freqs file 1
   -2|--file2 [FILE] : SNP freqs file 2
+  -f|--fasta [FILE] : reference fasta sequence file (optional)
   -o|--out   [STR]  : output prefix (default 'compare_SNP')
   -h|--help
 \n";
 
-my ($file1,$file2,$help);
+my ($file1,$file2,$fasta,$help);
 my $outfile = "compare_SNP";
 my $processed = 0;
 
 GetOptions (
   '1|file1=s' => \$file1,
   '2|file2=s' => \$file2,
+  'f|fasta:s' => \$fasta,
   'o|out:s'   => \$outfile,
   'h|help'    => \$help,
 );
@@ -30,8 +33,16 @@ GetOptions (
 die $usage if $help;
 die $usage unless ($file1 && $file2);
 
-my (%h1,%h2,%intersect,%uniq1,%uniq2);
+my (%seqlengths,%h1,%h2,%intersect,%uniq1,%uniq2);
 my ($mismatch,$uniq1,$uniq2) = (0,0,0);
+
+if ($fasta) {
+  print STDERR "[INFO] Getting sequence lengths from: $fasta...\n";
+  my $in = Bio::SeqIO ( -file => $fasta, -format => 'fasta' );
+  while ( my $seqobj = $in -> next_seq() ) {
+    $seqlengths{$seqobj->display_id()} = $seqobj->length();
+  }
+}
 
 ## parse SNPs in file1/2:
 open (my $FILE1, $file1) or die $!;
@@ -59,12 +70,31 @@ foreach (sort {ncmp($h1{$a}{chrom},$h1{$b}{chrom})} keys %h1) {
   if ( (exists($h2{$_})) and ($h1{$_}{chrom} eq $h2{$_}{chrom}) ) {
     ## check REF and ALT alleles are also the same:
     unless ( ($h1{$_}{ref} eq $h2{$_}{ref}) and ($h1{$_}{alt} eq $h2{$_}{alt}) ) {
-      print $MISMATCH "[INFO] SNP at position $h1{$_}{chrom}:$h1{$_}{pos} has $h1{$_}{ref}/$h1{$_}{alt} in $file1 but $h2{$_}{ref}/$h2{$_}{alt} in $file2 ($h2{$_}{chrom}:$h2{$_}{pos})\n";
+      print $MISMATCH join (
+        "\t",
+        $h1{$_}{chrom},
+        $seqlengths{chrom},
+        $h1{$_}{pos},
+        $h1{$_}{ref},
+        $h1{$_}{alt},
+        $h1{$_}{TC},
+        $h1{$_}{TR},
+        $h1{$_}{MAF},
+        $h2{$_}{chrom},
+        $h2{$_}{pos},
+        $h2{$_}{ref},
+        $h2{$_}{alt},
+        $h2{$_}{TC},
+        $h2{$_}{TR},
+        $h2{$_}{MAF},
+        "\n"
+      );
       $mismatch++;
     } else {
       print $INTERSECT join (
         "\t",
         $h1{$_}{chrom},
+        $seqlengths{chrom},
         $h1{$_}{pos},
         $h1{$_}{ref},
         $h1{$_}{alt},
@@ -86,6 +116,7 @@ foreach (sort {ncmp($h1{$a}{chrom},$h1{$b}{chrom})} keys %h1) {
     print $UNIQ1 join (
       "\t",
       $h1{$_}{chrom},
+      $seqlengths{chrom},
       $h1{$_}{pos},
       $h1{$_}{ref},
       $h1{$_}{alt},
@@ -105,6 +136,7 @@ foreach (sort {ncmp($h2{$a}{chrom},$h2{$b}{chrom})} keys %h2) {
     print $UNIQ2 join (
       "\t",
       $h2{$_}{chrom},
+      $seqlengths{chrom},
       $h2{$_}{pos},
       $h2{$_}{ref},
       $h2{$_}{alt},
