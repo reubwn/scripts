54c3808113150ccccfb1d6da2468fb4714b33848
reubwn <reubennowell@gmail.com>
2017-12-06 14:09:39 +0000

    update script

--- compare_SNPs.pl
+++ compare_SNPs.pl
@@ -48,7 +48,7 @@ open (my $FILE1, $file1) or die $!;
 while (<$FILE1>) {
   chomp;
   my @F = split /\s+/;
-  $h1{"$F[0].$F[1]"} = { 'chrom' => $F[0], 'pos' => $F[1], 'ref' => $F[2], 'alt' => $F[3], 'TC' => $F[5], 'TR' => $F[6], 'MAF' => $F[8] }; ##key= pos; val= %{chrom...}
+  $h1{"$F[0].$F[1]"} = { 'chrom' => $F[0], 'pos' => $F[1], 'ref' => $F[2], 'alt' => $F[3], 'TC' => $F[5], 'TR' => $F[6], 'MAF' => $F[9] }; ##key= pos; val= %{chrom...}
 }
 close $FILE1;
 open (my $FILE2, $file2) or die $!;
