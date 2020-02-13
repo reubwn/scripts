f37c605d7833b7fba42a17c4cc51b1d891e1fe84
Reuben Nowell <reubennowell@gmail.com>
2020-02-13 10:24:32 +0100

    update

--- BUSCO_collapse_multicopy.pl
+++ BUSCO_collapse_multicopy.pl
@@ -40,7 +40,7 @@ my @faa_files = glob ("$busco_path/augustus_output/extracted_proteins/*faa*");
 foreach my $faa_file (@faa_files) {
   print STDERR "\r[INFO] Extracting proteins from $faa_file"; $|=1;
   ## BUSCO name from filename
-  my $busco_id = basename($faa_file, qr/.faa/);
+  my $busco_id = basename($faa_file, qr/\.faa$ \.faa\.\d+$/);
   # (my $busco_id = $faa_file) =~ s/\.faa\.\d//;
   my $in = Bio::SeqIO -> new ( -file => $faa_file, -format => "fasta");
   while ( my $seq_obj = $in -> next_seq() ) {
