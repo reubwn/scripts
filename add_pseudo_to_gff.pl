7573126cb378f09bbaeda2c205bc53e468f76a04
Reuben Nowell <reubennowell@gmail.com>
2020-08-26 14:16:00 +0200

    update

--- add_pseudo_to_gff.pl
+++ add_pseudo_to_gff.pl
@@ -62,7 +62,7 @@ while (my $line = <$IN>) {
 
   ## find broken genes from list
   if ( $F[2] eq "gene" ) {
-    my $ID = $1 if ($F[8] =~ m/locus_tag=(.+?)/); ## inherit ID from locus_tag
+    my $ID = $1 if ($F[8] =~ m/locus_tag=(.+)?$/); ## inherit ID from locus_tag
     print STDERR "$ID\n" if ( $debug );
     if ( $broken_genes{$ID} ) {
       ## gene is broken
