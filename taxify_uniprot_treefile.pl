4009e1de70d98a56ee23bf9f8fb4c919d9445f33
Reuben Nowell <>
2021-04-26 11:37:47 +0100

    taxify_uniprot script

--- taxify_uniprot_treefile.pl
+++ taxify_uniprot_treefile.pl
@@ -14,32 +14,34 @@ SYNOPSIS:
 
 OPTIONS:
   -i|--infile     [FILE]   : input treefile
+  -f|--fofn       [FILE]   : fofn of treefiles, one per line
   -o|--out_suffix [FILE]   : suffix to be added to modified treefile ['.tax.treefile']
   -p|--taxdb      [STRING] : path to nodes.dmp and names.dmp tax files
-  -t|--taxlist    [FILE]   : UniProt taxid file, formatted 'uniprotid TAB taxid'
   -s|--speclist   [FILE]   : UniProt species identification codes file (https://www.uniprot.org/docs/speclist)
+  -t|--taxlist    [FILE]   : UniProt taxid file, formatted 'uniprotid TAB taxid' (optional)
   -d|--taxdepth   [STRING] : depth of taxonomy returned, can be 'phylum' or 'species' ['phylum']
   -n|--taxnumber           : include taxid integer in string? [no]
   -h|--help                : prints this help message
 \n";
 
-my ($infile,$path,$tax_list,$spec_list,$tax_number,$help);
+my ($infile,$fofn,$path,$tax_list,$spec_list,$tax_number,$help);
 my $out_suffix = "tax.treefile";
 my $tax_depth = "phylum";
 
 GetOptions (
-  'i|infile=s'  => \$infile,
+  'i|infile:s'  => \$infile,
+  'f|fofn:s'  => \$fofn,
   'o|out_suffix:s' => \$out_suffix,
   'p|taxdb=s'   => \$path,
-  't|taxlist=s' => \$tax_list,
   's|speclist=s' => \$spec_list,
+  't|taxlist:s' => \$tax_list,
   'd|depth:i'   => \$tax_depth,
   'n|taxnumber' => \$tax_number,
   'h|help'      => \$help,
 );
 
 die $usage if $help;
-die $usage unless ($infile && $tax_list && $path);
+die $usage unless (($infile || $fofn) && $spec_list && $path);
 
 ## parse nodes and names:
 my (%nodes_hash, %names_hash, %rank_hash);
@@ -74,89 +76,102 @@ if (-e "$path/merged.dmp") {
 }
 print STDERR "[INFO] Nodes parsed: ".commify(scalar(keys %nodes_hash))."\n";
 
-## parse UniProt species list:
-## use this FIRST and then grep from taxid file if no match is found - much faster!
-
 ## parse treefile:
 print STDERR "[INFO] Getting UniProt IDs from '$infile'...\n";
 
 my %tax_hash;
 my @uniprot_strings;
 
-open (my $TREEFILE_READ, $infile) or die $!;
-while (<$TREEFILE_READ>) {
-  ## this regex *should* capture most UniProt accessions and gene IDs, plus the (_\d+\-\d+){0,1} suffix added by hmmalign/IQTREE
-  while ($_ =~ m/(\w{1,5}_\w{1,5}(?<=[A-Z])(_\d+\-\d+){0,1}|[OPQ][0-9][A-Z0-9]{3}[0-9]_[A-Z0-9]{1,5}(_\d+\-\d+){0,1}|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}_[A-Z0-9]{1,5}(_\d+\-\d+){0,1})/g) {
-    push (@uniprot_strings, $1);
+my @treefiles;
+if ( $infile ) {
+  push (@treefiles, $infile);
+  print STDERR "[INFO] Working with one treefile: '$infile'\n";
+} elsif ( $fofn ) {
+  open (my $FOFN, $fofn) or die $!;
+  while (my $line = <$FOFN>) {
+    chomp $line;
+    push (@treefiles, $line);
   }
-  ## explanation
-  ## m/(\w{1,5}_\w{1,5}(?<=[A-Z]) ## match gene ID followed by species ID, requires at least one letter e.g. CASP2_RAT
-  ## (_\d+\-\d+){0,1} ## maybe match hmmalign stuff
-  ## |[OPQ][0-9][A-Z0-9]{3}[0-9]_[A-Z0-9]{1,5}(_\d+\-\d+){0,1}|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}_[A-Z0-9]{1,5}(_\d+\-\d+){0,1})/g ## or match UniProt ID
+  close $FOFN;
+  print STDERR "[INFO] Number of treefiles in '$fofn': ".scalar(@treefiles)."\n";
 }
-close $TREEFILE_READ;
-print STDERR "[INFO] Number of UniProt IDs scooped from treefile: ".commify(scalar(@uniprot_strings))."\n";
 
-foreach my $orig_string (@uniprot_strings) {
-  ## default is the original ID
-  my $taxid;
-  my @a = split ("_", $orig_string);
-  my $replace_string = join ("_", $a[0], $a[1]); ## default is to include the UniProt species code
-  ## try to grep from speclist using species ID first for speed
-  my $match = `grep -m1 -wF $a[1] $spec_list`;
-  if ( $match ) {
-    my @b = split (m/\s+/, $match);
-    $taxid = $b[2];
-    $taxid =~ s/://;
-  } else {
-    ## else parse UniProt accession for taxid
-    $match = `grep -m1 -wF $a[0] $tax_list`;
-    my @b = split (m/\s+/, $match);
-    $taxid = $b[1];
+foreach my $current_file (@treefiles) {
+  open (my $TREEFILE_READ, $current_file) or die $!;
+  while (<$TREEFILE_READ>) {
+    ## this regex *should* capture most UniProt accessions and gene IDs, plus the (_\d+\-\d+){0,1} suffix added by hmmalign/IQTREE
+    while ($_ =~ m/(\w{1,5}_\w{1,5}(?<=[A-Z])(_\d+\-\d+){0,1}|[OPQ][0-9][A-Z0-9]{3}[0-9]_[A-Z0-9]{1,5}(_\d+\-\d+){0,1}|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}_[A-Z0-9]{1,5}(_\d+\-\d+){0,1})/g) {
+      push (@uniprot_strings, $1);
+    }
+    ## explanation
+    ## m/(\w{1,5}_\w{1,5}(?<=[A-Z]) ## match gene ID followed by species ID, requires at least one letter e.g. CASP2_RAT
+    ## (_\d+\-\d+){0,1} ## maybe match hmmalign stuff
+    ## |[OPQ][0-9][A-Z0-9]{3}[0-9]_[A-Z0-9]{1,5}(_\d+\-\d+){0,1}|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}_[A-Z0-9]{1,5}(_\d+\-\d+){0,1})/g ## or match UniProt ID
   }
-  ## pull taxonomy string from taxdb
-  if ( $taxid ) {
-    if (($taxid =~ m/\d+/) && (check_taxid_has_parent($taxid) == 0)) {
-      if ($tax_depth eq "species") {
-        print STDERR " --> " . join (" ", join("_",$a[0],$a[1]), $taxid, tax_walk_to_get_rank_to_species($taxid)) . "\n";
-        if ( $tax_number ) {
-          $replace_string = join ("_", $a[0], tax_walk_to_get_rank_to_species($taxid), $taxid);
+  close $TREEFILE_READ;
+  print STDERR "[INFO] Number of UniProt IDs scooped from treefile '$current_file': ".commify(scalar(@uniprot_strings))."\n";
+
+  foreach my $orig_string (@uniprot_strings) {
+    ## default is the original ID
+    my $taxid;
+    my @a = split ("_", $orig_string);
+    my $replace_string = join ("_", $a[0], $a[1]); ## default is to include the UniProt species code
+    ## try to grep from speclist using species ID first for speed
+    my $match = `grep -m1 -wF $a[1] $spec_list`;
+    if ( $match ) {
+      my @b = split (m/\s+/, $match);
+      $taxid = $b[2];
+      $taxid =~ s/://;
+    } else {
+      ## else parse UniProt accession for taxid
+      $match = `grep -m1 -wF $a[0] $tax_list`;
+      my @b = split (m/\s+/, $match);
+      $taxid = $b[1];
+    }
+    ## pull taxonomy string from taxdb
+    if ( $taxid ) {
+      if (($taxid =~ m/\d+/) && (check_taxid_has_parent($taxid) == 0)) {
+        if ($tax_depth eq "species") {
+          print STDERR " --> " . join (" ", join("_",$a[0],$a[1]), $taxid, tax_walk_to_get_rank_to_species($taxid)) . "\n";
+          if ( $tax_number ) {
+            $replace_string = join ("_", $a[0], tax_walk_to_get_rank_to_species($taxid), $taxid);
+          } else {
+            $replace_string = join ("_", $a[0], tax_walk_to_get_rank_to_species($taxid));
+          }
         } else {
-          $replace_string = join ("_", $a[0], tax_walk_to_get_rank_to_species($taxid));
+          print STDERR " --> " . join (" ", join("_",$a[0],$a[1]), $taxid, tax_walk_to_get_rank_to_phylum($taxid)) . "\n";
+          if ( $tax_number ) {
+            $replace_string = join ("_", $a[0], tax_walk_to_get_rank_to_phylum($taxid), $taxid);
+          } else {
+            $replace_string = join ("_", $a[0], tax_walk_to_get_rank_to_phylum($taxid));
+          }
         }
       } else {
-        print STDERR " --> " . join (" ", join("_",$a[0],$a[1]), $taxid, tax_walk_to_get_rank_to_phylum($taxid)) . "\n";
-        if ( $tax_number ) {
-          $replace_string = join ("_", $a[0], tax_walk_to_get_rank_to_phylum($taxid), $taxid);
-        } else {
-          $replace_string = join ("_", $a[0], tax_walk_to_get_rank_to_phylum($taxid));
-        }
+        print STDERR " --> " . join (" ", join("_",$a[0],$a[1]), "Invalid taxid") . "\n";
       }
     } else {
-      print STDERR " --> " . join (" ", join("_",$a[0],$a[1]), "Invalid taxid") . "\n";
+      print STDERR " --> " . join (" ", join("_",$a[0],$a[1]), "No taxid found") . "\n";
     }
-  } else {
-    print STDERR " --> " . join (" ", join("_",$a[0],$a[1]), "No taxid found") . "\n";
+    ## replacement string hash
+    $tax_hash{$orig_string} = $replace_string;
   }
-  ## replacement string hash
-  $tax_hash{$orig_string} = $replace_string;
+
+  ## set up regex
+  my $regex = join ("|", keys %tax_hash);
+  $regex = qr/$regex/;
+
+  ## open treefile again and make the substitution:
+  print STDERR "[INFO] Printing new tree to '$current_file.$out_suffix'...\n";
+
+  open (my $TREEFILE_WRITE, $current_file) or die $!;
+  open (my $OUT, ">$current_file.$out_suffix") or die $!;
+  while (my $tree = <$TREEFILE_WRITE>) {
+    $tree =~ s/($regex)/$tax_hash{$1}/g;
+    print $OUT $tree;
+  }
+  close $TREEFILE_WRITE;
+  close $OUT;
 }
-
-## set up regex
-my $regex = join ("|", keys %tax_hash);
-$regex = qr/$regex/;
-
-## open treefile again and make the substitution:
-print STDERR "[INFO] Printing new tree to '$infile.$out_suffix'...\n";
-
-open (my $TREEFILE_WRITE, $infile) or die $!;
-open (my $OUT, ">$infile.$out_suffix") or die $!;
-while (my $tree = <$TREEFILE_WRITE>) {
-  $tree =~ s/($regex)/$tax_hash{$1}/g;
-  print $OUT $tree;
-}
-close $TREEFILE_WRITE;
-close $OUT;
 
 print STDERR "[INFO] Done " . `date`;
 
