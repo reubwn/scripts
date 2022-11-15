e92f779be54ca4e409326959c75f0385839af135
Reuben Nowell <>
2022-11-15 21:07:42 +0000

    update find_coding_frame.pl

--- find_coding_frame.pl
+++ find_coding_frame.pl
@@ -38,7 +38,7 @@ die $usage unless ( $aa_file && $dna_file );
 my %prot_hash;
 my $aa_file_fh = Bio::SeqIO -> new ( -file => $aa_file, -format => "fasta" );
 while ( my $seq_obj = $aa_file_fh -> next_seq() ) {
-  $prot_hash{$seq_obj->display_id()} = $seq_obj->seq();
+  $prot_hash{$seq_obj->display_id()} = $seq_obj;
 }
 print STDERR "[INFO] Got ".scalar(keys %prot_hash)." protein seqs from '$aa_file'\n";
 
@@ -46,13 +46,13 @@ print STDERR "[INFO] Got ".scalar(keys %prot_hash)." protein seqs from '$aa_file
 my %transcripts_hash;
 my $dna_file_fh = Bio::SeqIO -> new ( -file => $dna_file, -format => "fasta" );
 while ( my $seq_obj = $dna_file_fh -> next_seq() ) {
-  $transcripts_hash{$seq_obj->display_id()} = $seq_obj->seq();
+  $transcripts_hash{$seq_obj->display_id()} = $seq_obj;
 }
 print STDERR "[INFO] Got ".scalar(keys %transcripts_hash)." protein seqs from '$dna_file'\n";
 
 foreach my $gid (keys %prot_hash) {
-  my $pseq = $prot_hash{$gid};
-  my $dseq = $transcripts_hash{$gid};
-  my $dseq_translation = $dseq -> translate();
-  print $gid . "\t" . $dseq_translation . "\n";
+  my $pseq_obj = $prot_hash{$gid};
+  my $dseq_obj = $transcripts_hash{$gid};
+  my $dseq_translation_obj = $dseq_obj -> translate();
+  print $gid . "\t" . $dseq_translation->seq() . "\n";
 }
