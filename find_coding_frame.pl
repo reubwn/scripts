bb8eac4e627a45e4a517ecd284141465dc986f57
Reuben Nowell <>
2022-11-15 21:03:58 +0000

    add find_coding_frame.pl

--- find_coding_frame.pl
+++ find_coding_frame.pl
@@ -12,6 +12,7 @@ use Data::Dumper;
 my $usage = "
 SYNOPSIS
   Use for trimming fragmented CDSs to the correct reading frame.
+  Protein names must match cDNA (transcripts) names exactly.
 
 OPTIONS [*required]
   -a|--aa       *[FILE] : aa sequences (fasta format)
@@ -33,10 +34,25 @@ GetOptions (
 die $usage if ( $help );
 die $usage unless ( $aa_file && $dna_file );
 
-## parse protein seqs
+## get protein seqs
 my %prot_hash;
 my $aa_file_fh = Bio::SeqIO -> new ( -file => $aa_file, -format => "fasta" );
 while ( my $seq_obj = $aa_file_fh -> next_seq() ) {
   $prot_hash{$seq_obj->display_id()} = $seq_obj->seq();
 }
 print STDERR "[INFO] Got ".scalar(keys %prot_hash)." protein seqs from '$aa_file'\n";
+
+## get nuc seqs
+my %transcripts_hash;
+my $dna_file_fh = Bio::SeqIO -> new ( -file => $dna_file, -format => "fasta" );
+while ( my $seq_obj = $dna_file_fh -> next_seq() ) {
+  $transcripts_hash{$seq_obj->display_id()} = $seq_obj->seq();
+}
+print STDERR "[INFO] Got ".scalar(keys %transcripts_hash)." protein seqs from '$dna_file'\n";
+
+foreach my $gid (keys %prot_hash) {
+  my $pseq = $prot_hash{$gid};
+  my $dseq = $transcripts_hash{$gid};
+  my $dseq_translation = $dseq -> translate();
+  print $gid . "\t" . $dseq_translation . "\n";
+}
