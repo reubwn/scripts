351bea4dfe06e31a0cbc0327c28e4541b29e1296
Reuben Nowell <>
2023-05-17 11:23:57 +0100

    update orthofinder_groups2phyletic.pl

--- orthofinder_groups2phyletic.pl
+++ orthofinder_groups2phyletic.pl
@@ -50,15 +50,58 @@ if (scalar(@fastas) == 0) {
 }
 
 print STDERR "[INFO] Reading sequences from:\n";
+my %species_hash;
 foreach (@fastas){
   print STDERR colored($_, 'white on_blue') . "\n";
   my $in = Bio::SeqIO->new ( -file => $_, -format => "fasta" );
   while ( my $seq_obj = $in->next_seq() ){
-    $seq_hash{($seq_obj->display_id())} = ($seq_obj->seq());
+    my $seq_id = $seq_obj->display_id();
+    my $species_id = split("\|", $seq_id)[0];
+    $seq_hash{$seq_id} = ();
+    $species_hash{$species_id}++;
   }
 }
 print STDERR "[INFO] Read in ".commify(scalar(keys %seq_hash))." sequences from ".commify(scalar(@fastas))." files\n\n";
+foreach (keys %species_hash) {
+  print "$_\n";
+}
 
+## open Orthogroups
+my %orthogroups;
+my %seen_in_orthogroups;
+
+# matrix looks like
+# >species1
+# 00010101100111101101111
+# where cols are OGs and membership is collapsed to 1/0
+
+open (my $GROUPS, $orthogroups_file) or die $!;
+while (my $line = <$GROUPS>) {
+  chomp $line;
+  my @a = split(/\:\s+/, $line);
+  my @b = split(/\s+/, $a[1]);
+  foreach (@b) {
+    if ($annot) {
+      if ($annot_hash{$_}) { ## genes with no hit to uniref are not in $annot
+        if ($annot_hash{$_}{category} eq "OUTGROUP") { ## only write annotations for HGTc genes
+          print $OUT ">$_ ";
+          print $OUT join (":", $annot_hash{$_}{hU}, $annot_hash{$_}{AI}, $annot_hash{$_}{category}, $annot_hash{$_}{CHS}, $annot_hash{$_}{tax});
+          print $OUT "\n";
+        }
+      } else {
+        print $OUT ">$_\n";
+      }
+      print $OUT "$seq_hash{$_}\n";
+    } else {
+      print $OUT ">$_\n$seq_hash{$_}\n";
+    }
+  }
+  close $OUT;
+  print STDERR "\r[INFO] Working on OG \#$.: $a[0]"; $|=1;
+}
+close $GROUPS;
+
+print STDERR "\n[INFO] Finished on ".`date`."\n";
 
 
 
